2fbeb5053e2ebc00b4f31eaac6206b978b0f6409
mukesh <mukesh.techy@gmail.com>
2018-01-28 20:50:45 +0530

    Application Context Change

---
+++ src/main/java/com/demo/controller/DataSourceController.java
@@ -0,0 +1,26 @@
+package com.demo.controller;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.repository.JdbcDataSource;
+
+@RestController
+public class DataSourceController implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+	@RequestMapping(value = "/db-source")
+	public JdbcDataSource getDataSource() {
+		//applicationContext = new ClassPathXmlApplicationContext("jdbc\\jdbcContext.xml");
+		JdbcDataSource jdbcDataSource = applicationContext.getBean("jdbcDataSource", JdbcDataSource.class);
+		return jdbcDataSource;
+
+	}
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+		
+	}
+}
---
+++ src/main/java/com/demo/repository/JdbcDataSource.java
@@ -0,0 +1,58 @@
+package com.demo.repository;
+
+public class JdbcDataSource {
+
+	private String driverClassName;
+	private String url;
+	private String username;
+	private String password;
+	private String maxActive;
+	private String maxIdle;
+	private String validationQuery;
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMaxActive() {
+		return maxActive;
+	}
+	public void setMaxActive(String maxActive) {
+		this.maxActive = maxActive;
+	}
+	public String getMaxIdle() {
+		return maxIdle;
+	}
+	public void setMaxIdle(String maxIdle) {
+		this.maxIdle = maxIdle;
+	}
+	public String getValidationQuery() {
+		return validationQuery;
+	}
+	public void setValidationQuery(String validationQuery) {
+		this.validationQuery = validationQuery;
+	}
+	
+	
+	
+	
+}
